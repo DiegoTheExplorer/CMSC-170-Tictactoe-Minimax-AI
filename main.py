26d4d82ddcdb57c5662c6149408cdee176056941
Diego Villamil <dmvillamil@up.edu.ph>
2024-01-03 13:23:44 +0800

    Draw state check added

--- main.py
+++ main.py
@@ -17,39 +17,54 @@ def disableAllBtns(frame):
   return
 
 #TICTACTOE FUNCTIONS
-def isWinState(gameState):
+def isEndState(gameState):
 
-  #Checking rows and columns
+  #Checking rows and columns for a win
   for i in range(0,3):
     rChk = set(gameState[i])
     cChk = set(gameState[:,i])
 
     if ((len(rChk) == 1 or len(cChk) == 1)):
-      return True
+      return 1
   
-  #Checking diagonals
+  #Checking diagonals for a win
     diag1 = set([gameState[0,0],gameState[1,1],gameState[2,2]])
     diag2 = set([gameState[0,2],gameState[1,1],gameState[2,0]])
 
     if (len(set(diag1)) == 1 or len(set(diag2)) == 1):
-      return True
-  return False
+      return 1
+  
+  #Checking for a draw
+    drawStateSet = {0,1}
+    gameStateSet = set()
+    for i in range(0,3):
+      for j in range(0,3):
+        gameStateSet.add(gameState[i,j])
+    if(drawStateSet == gameStateSet):
+      return 2
+    
+  return 0
 
 def buttonClick(self, gameState, root, frame, turnCount):
 
   row = self.grid_info()['row']
   col = self.grid_info()['column']
   mark = 'X' if turnCount % 2 == 0 else 'O'
-  #10 is 'O" and 20 is 'X'
+  #0 is 'O' and 1 is 'X'
 
   #Mark current button and set as disabled
   self.config(text=mark, state='disabled')
-  gameState[row][col] = 10 if mark == 'O' else 20
+  gameState[row][col] = 0 if mark == 'O' else 1
   turnCount += 1
 
-  #Check if winstate
-  if isWinState(gameState):
+  #Check if end state
+  currState = isEndState(gameState)
+  if currState == 1:
     print("PLAYER WINS")
+    disableAllBtns(frame)
+    return
+  elif currState == 2:
+    print("GAME DRAW")
     disableAllBtns(frame)
     return
 
@@ -64,14 +79,19 @@ def buttonClick(self, gameState, root, frame, turnCount):
     rBtn = frame.grid_slaves(rRow,rCol)
     if rBtn[0]['state'] == 'normal':
       rBtn[0].config(text=mark,state='disabled')
-      gameState[rRow][rCol] = 10 if mark == 'O' else 20
+      gameState[rRow][rCol] = 0 if mark == 'O' else 1
       turnDone = False
       
   turnCount += 1
 
-  #Check if winstate
-  if isWinState(gameState):
+  #Check if end state
+  currState = isEndState(gameState)
+  if currState == 1:
     print("COMPUTER WINS")
+    disableAllBtns(frame)
+    return
+  elif currState == 2:
+    print("GAME DRAW")
     disableAllBtns(frame)
     return
 
@@ -100,7 +120,7 @@ def initButtons(gameState, root, frame, turnCount, isPlayerTurn):
     rCol = random.randint(0,2)
     rBtn = frame.grid_slaves(rRow,rCol)
     rBtn[0].config(text="X",state='disabled')
-    gameState[rRow][rCol] = 20
+    gameState[rRow][rCol] = 1
     turnCount += 1
 
   return
@@ -113,7 +133,7 @@ frame = tk.Frame(root, width = 600, height = 600)
 frame.grid(columnspan = 3, rowspan = 3)
 
 #Tictactoe Variables
-gameState = np.array([[1,2,3],[4,5,6],[7,8,9]])
+gameState = np.array([[3,4,5],[6,7,8],[9,10,11]])
 turnCount = 1
 isPlayerTurn = random.randint(0,1)
 isPlayerTurn = True if isPlayerTurn == 1 else False
