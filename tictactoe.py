1edc7ca68cd945623a3c1bd8dd67005e569e57c6
Diego Villamil <dmvillamil@up.edu.ph>
2024-01-04 13:07:58 +0800

    restarted on tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -6,8 +6,8 @@ import tkinter.messagebox as message
 from functools import partial
 import random 
 
-#User imports
-from tttAI import *
+#Global Variables
+turnCount = 1
 
 #UI FUNCTIONS
 def disableAllBtns(frame):
@@ -15,17 +15,49 @@ def disableAllBtns(frame):
     btn['state'] = 'disabled'
   return
 
-def buttonClick(self, gameState, root, frame, turnCount, aiMark):
+# Checks if the game is done at the current state
+# 0 if game is not over
+# 1 if X wins
+# 2 if O wins
+# 3 if the game is a draw
+def isEndState(gameState):
 
+  #Checking rows and columns for a win
+  for i in range(0,3):
+    rChk = set([gameState[i][0],gameState[i][1],gameState[i][2]])
+    cChk = set([gameState[0][i],gameState[1][i],gameState[2][i]])
+
+    if (len(rChk) == 1 or len(cChk) == 1):
+      return 1
+  
+  #Checking diagonals for a win
+    diag1 = set([gameState[0][0],gameState[1][1],gameState[2][2]])
+    diag2 = set([gameState[0][2],gameState[1][1],gameState[2][0]])
+
+    if (len((diag1)) == 1 or len((diag2)) == 1):
+      return 1
+  
+  #Checking for a draw
+    drawStateSet = {0,1}
+    gameStateSet = set()
+    for i in range(0,3):
+      for j in range(0,3):
+        gameStateSet.add(gameState[i][j])
+    if(drawStateSet == gameStateSet):
+      return 2
+    
+  return 0
+
+def buttonClick(self, gameState, root, frame):
+  global turnCount
   row = self.grid_info()['row']
   col = self.grid_info()['column']
   mark = 'O' if turnCount % 2 == 0 else 'X'
-  #0 is 'O' and 1 is 'X'
 
   #Mark current button and set as disabled
   self.config(text=mark, state='disabled')
   gameState[row][col] = 0 if mark == 'O' else 1
-  turnCount += 1
+  turnCount = turnCount + 1
 
   #Check if end state
   currState = isEndState(gameState)
@@ -38,40 +70,10 @@ def buttonClick(self, gameState, root, frame, turnCount, aiMark):
     disableAllBtns(frame)
     return
 
-  #Minimax AI Start 
-  # print("ai turn")
-  mark = "O" if turnCount % 2 == 0 else "X"
-  val = 0 if mark == 'O' else 1
-  inv = 1 if val == 0 else 0
-
-  actionList = possibleActionsWithInd(gameState,val)
-  for action in actionList:
-    action[0] = minimax(action[0],False,inv,aiMark)
-
-  aiMove = max(actionList,key=lambda x:x[0])
-  gameState[aiMove[1]][aiMove[2]] = val
-  currBtn = frame.grid_slaves(aiMove[1],aiMove[2])
-  currBtn[0].config(text=mark,state='disabled')
-  #Minimax AI end
-
-  #Check if end state
-  currState = isEndState(gameState)
-  if currState == 2:
-    print("COMPUTER WINS")
-    disableAllBtns(frame)
-    return
-  elif currState == 3:
-    print("GAME DRAW")
-    disableAllBtns(frame)
-    return
-
-  turnCount += 1
-  return
-
 #INIT FUNCTIONS
 #creates a 3x3 grid of buttons and assigns buttonClick to be called when a button is clicked
-def initButtons(gameState, root, frame, turnCount, isPlayerTurn, aiMark):
-
+def initButtons(gameState, root, frame, isPlayerTurn):
+  global turnCount
   for row in range(0,3):
     for col in range(0,3):
         button = tk.Button(frame,
@@ -82,21 +84,17 @@ def initButtons(gameState, root, frame, turnCount, isPlayerTurn, aiMark):
                           width = 10, 
                           height = 5)
         button.grid(row = row, column = col, sticky="nesw")
-        button.config(command= partial(buttonClick, button, gameState, root, frame, turnCount, aiMark))
+        button.config(command= partial(buttonClick, button, gameState, root, frame))
 
-  aiMark.set("O")
-  isPlayerTurn = True #dbg
   #AI does a random move when AI goes first
   if not isPlayerTurn:
-    aiMark.set("X")
     rRow = random.randint(0,2)
     rCol = random.randint(0,2)
     rBtn = frame.grid_slaves(rRow,rCol)
     rBtn[0].config(text="X",state='disabled')
     gameState[rRow][rCol] = 1
-    turnCount += 1
+    turnCount = turnCount + 1
 
-  return
 
 #APP START
 root = tk.Tk()
@@ -106,17 +104,12 @@ frame = tk.Frame(root, width = 600, height = 600)
 frame.grid(columnspan = 3, rowspan = 3)
 
 #Tictactoe Variables
-# gameState = [[4,5,0],
-#             [1,0,9],
-#             [1,11,12]]
 gameState = [[4,5,6],
             [7,8,9],
             [10,11,12]]
-turnCount = 1
-aiMark = tk.StringVar(root)
 isPlayerTurn = random.randint(0,1)
 isPlayerTurn = True if isPlayerTurn == 1 else False
 
-initButtons(gameState,root,frame,turnCount,isPlayerTurn,aiMark)
+initButtons(gameState,root,frame,isPlayerTurn,)
 
 root.mainloop()
--- tttAI.py
+++ tttAI.py
@@ -1,34 +1,55 @@
 from copy import deepcopy
 
-#Checks if the game is done at the current state
-#Game is not over = 0
-#Game has a winner = 1
-#Game is a draw = 2
+# Checks if the game is done at the current state
+# 0 if game is not over
+# 1 if X wins
+# 2 if O wins
+# 3 if the game is a draw
 def isEndState(gameState):
 
   #Checking rows and columns for a win
   for i in range(0,3):
-    rChk = set(gameState[i])
-    cChk = set(gameState[:,i])
+    rChk = set([gameState[i][0],gameState[i][1],gameState[i][2]])
+    cChk = set([gameState[0][i],gameState[1][i],gameState[2][i]])
 
-    if ((len(rChk) == 1 or len(cChk) == 1)):
-      return 1
+    if (len(rChk) == 1):
+      for e in rChk:
+        if e == 1:
+          return 1
+        else:
+          return 2
+    if (len(cChk) == 1):
+      for e in rChk:
+        if e == 1:
+          return 1
+        else:
+          return 2
   
   #Checking diagonals for a win
-    diag1 = set([gameState[0,0],gameState[1,1],gameState[2,2]])
-    diag2 = set([gameState[0,2],gameState[1,1],gameState[2,0]])
+    diag1 = set([gameState[0][0],gameState[1][1],gameState[2][2]])
+    diag2 = set([gameState[0][2],gameState[1][1],gameState[2][0]])
 
-    if (len(set(diag1)) == 1 or len(set(diag2)) == 1):
-      return 1
+    if (len((diag1)) == 1):
+      for e in diag1:
+        if e == 1:
+          return 1
+        else:
+          return 2
+    if (len((diag2)) == 1):
+      for e in diag2:
+        if e == 1:
+          return 1
+        else:
+          return 2
   
   #Checking for a draw
     drawStateSet = {0,1}
     gameStateSet = set()
     for i in range(0,3):
       for j in range(0,3):
-        gameStateSet.add(gameState[i,j])
+        gameStateSet.add(gameState[i][j])
     if(drawStateSet == gameStateSet):
-      return 2
+      return 3
     
   return 0
 
@@ -41,7 +62,6 @@ def possibleActions(gameState, val):
         newState = deepcopy(gameState)
         newState[i][j] = val
         actionList.append(newState)
-
   return actionList
 
 def possibleActionsWithInd(gameState, val):
@@ -54,27 +74,29 @@ def possibleActionsWithInd(gameState, val):
         actionList.append([newState,i,j])
   return actionList
 
-def minimax(gameState, maximizer, val):
+def minimax(gameState, maximizer, val, aiMark):
   currState = isEndState(gameState)
   if (currState == 1):
-    eval = -1 if maximizer else 1
+    eval = 1 if maximizer else -1
+    print("**********")
+    for row in gameState:
+      print(row)
+    print("eval: ",eval)
     return eval
-  elif (currState == 2):
-    return 0
 
   actions = possibleActions(gameState,val)
   if (maximizer):
-    maxEval = float("-inf") #m
+    maxEval = -2
     for resState in actions:
       val = 1 if val == 0 else 0
-      eval = minimax(resState, False, val)
+      eval = minimax(resState, False, val, aiMark)
       maxEval = max(maxEval,eval)
-      return maxEval
+    return maxEval
 
   else:
-    minEval = float("inf") #m
+    minEval = 2
     for resState in actions:
       val = 1 if val == 0 else 0
-      eval = minimax(resState, True, val)
+      eval = minimax(resState, True, val, aiMark)
       minEval = min(minEval,eval)
-      return minEval
+    return minEval
